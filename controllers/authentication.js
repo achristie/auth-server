c72a9b5d570d181689231b73b9c1e66ed2fdf03e
Andrew Christie <awchristie86@gmail.com>
2016-07-04 23:05:51 -0400

    init

---
+++ controllers/authentication.js
@@ -0,0 +1,43 @@
+const User = require('../models/user');
+const jwt = require('jwt-simple');
+const config = require('../config');
+
+
+function tokenForUser(user) {
+  console.log(user);
+  const timestamp = new Date().getTime();
+  return jwt.encode({ sub: user._id, iat: timestamp }, config.secret);
+}
+
+exports.signup = function (req, res, next) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  if (!email || !password) {
+    return res.status(422).send({error: 'You must provide an email and password'});
+  }
+
+  User.findOne({ email: email }, function (err, user) {
+    if (err) { 
+      return next(err);
+    }
+
+    if (user) {
+      return res.status(422).send({error: 'Email is in use'});
+    }
+
+    const u = new User({
+      email: email,
+      password: password
+    });
+
+    u.save(function (err) {
+      if (err) {
+        return next(err);
+      }
+
+      res.json({ token: tokenForUser(u) });
+    });
+
+  });
+};
